c018862aff709601bc398e415eca8211c0732fb7
max810 <maksym.bekuzarov@gmail.com>
2021-11-16 14:38:51 +0400

    Original code, added wandb.

---
+++ eval.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  7 14:31:35 2021
+
+@author: mustansar
+"""
+
+python scripts/evaluate_mpii.py --arch=hg2 --image-path=dataset --model-file checkpoint/hg2/checkpoint.pth.tar
---
+++ train.sh
@@ -0,0 +1,12 @@
+export PYTHONPATH=/home/maksym.bekuzarov/CV701/CV701_ass4/src:$PYTHONPATH
+
+python scripts/train_mpii.py \
+    --arch=hg2 \
+    --image-path=dataset \
+    --checkpoint=checkpoint/hg2 \
+    --epochs=30 \
+    --train-batch=24 \
+    --workers=24 \
+    --test-batch=24 \
+    --lr=1e-3 \
+    --schedule 15 17
